e4a197ea96c78c3139cf9b155d39d5ccecd02f68
Rashed.Qazizada <rashed.qazizada@softhouse.se>
2022-09-21 10:55:48 +0200

    updates seeding and installed faker

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { faker } from '@faker-js/faker';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
@@ -32,4 +36,37 @@ async function addNewCustomerWithPost() {
   }
 }
 
-addNewCustomerWithPost();
+// addNewCustomerWithPost();
+
+async function addNewCustomersWithPosts() {
+  try {
+    const username = faker.internet.userName();
+    const newCustomer = await prisma.customers.upsert({
+      where: { username },
+      update: {},
+      create: {
+        name: faker.name.fullName(),
+        username,
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        posts: {
+          create: {
+            title: faker.lorem.sentence(3),
+            body: faker.lorem.sentence(8),
+          },
+        },
+      },
+    });
+
+    console.log('Create 1 customer with 2 post: ', newCustomer);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+addNewCustomersWithPosts();
