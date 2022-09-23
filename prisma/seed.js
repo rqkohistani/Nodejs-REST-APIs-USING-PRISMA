aa9c881b77ce3f8905b567e2686bafe58f9f7c17
Rashed Qazizada <rashed.qazizada@softhouse.se>
2022-09-23 11:02:20 +0200

    updates seeding script

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-console */
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { faker } from '@faker-js/faker';
+import bcrypt from 'bcrypt';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
@@ -69,4 +70,44 @@ async function addNewCustomersWithPosts() {
   }
 }
 
+const createAdmin = async () => {
+  try {
+    const admin = await prisma.user.create({
+      data: {
+        name: 'Admin',
+        email: 'admin@gmail.com',
+        password: bcrypt.hashSync('password', 10),
+        userRole: 'admin',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    });
+    console.log('Create 1 admin: ', admin);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+const createUser = async () => {
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name: 'User',
+        email: 'user@gmail.com',
+        password: bcrypt.hashSync('password', 10),
+        userRole: 'user',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    });
+    console.log('Create 1 user: ', user);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+createAdmin();
+createUser();
 addNewCustomersWithPosts();
