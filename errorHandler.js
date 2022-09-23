510582a248e8533e0c52149ee79fb352446c1d4e
Rashed Qazizada <rashed.qazizada@softhouse.se>
2022-09-23 11:28:05 +0200

    updates errorHandler

--- errorHandler.js
+++ errorHandler.js
@@ -1,37 +1,63 @@
 /* eslint-disable no-console */
-
 import { DEFAULT_ERROR } from './constants';
 import { capitalizeFirstLetter, indexOfEnd } from './utils/helper.util';
 
-const databaseErrors = {
-  1062: 'Duplicate entry',
-  1452: 'Cannot add or update a child row: a foreign key constraint fails',
-  1451: 'Cannot delete or update a parent row: a foreign key constraint fails',
+const dbErrors = {
+  P2002: (tableName, prismaMetaData) => ({
+    status: 409,
+    message: `${capitalizeFirstLetter(tableName)} with provided ${prismaMetaData?.target} already exists.`,
+  }),
+  P2003: (tableName, prismaMetaData) => ({
+    status: 409,
+    message: `${capitalizeFirstLetter(tableName)} with provided ${prismaMetaData?.field_name} not found.`,
+  }),
+  P2025: (tableName) => ({
+    status: 404,
+    message: `${capitalizeFirstLetter(tableName)} not found.`,
+  }),
+};
+
+const getTableNameFromPrismaErrorMessage = (message) => {
+  /* 
+    If the item (fx a user) already exists in db,
+    message will contain the string 
+      "Invalid `prisma.user.create()` invocation".
+    The name of the variable after "prisma." is the table name. 
+  */
+  const tableNameIndex = indexOfEnd(message, 'prisma.');
+  if (tableNameIndex < 0) return null;
+
+  const [tableName] = message.substring(tableNameIndex).split(/[. (]/);
+  return tableName;
 };
 
 const getDbError = (error) => {
-  if (!Object.keys(databaseErrors).includes(error?.code)) return null;
+  // Prisma errors contains a "code" key
+  if (!Object.keys(dbErrors).includes(error?.code)) return null;
+
   const { message } = error;
   if (typeof message !== 'string') return null;
 
-  const tableName = message.substring(indexOfEnd(message, 'CONSTRAINT `'), indexOfEnd(message, '` FOREIGN KEY'));
-  const errorType = databaseErrors[error.code];
-  return `${errorType} ${tableName}`;
+  const tableName = getTableNameFromPrismaErrorMessage(message);
+  if (typeof tableName !== 'string') return null;
+
+  return dbErrors[error.code](tableName, error.meta);
 };
 
+/**
+ * Returns custom message based on known errors.
+ * @param {} error
+ * @returns
+ */
 const getDerivedError = (error) => {
   if (error.type === 'entity.parse.failed') return { status: 400, message: 'Request body contains invalid JSON' };
 
-  const databaseErrors = getDbError(error);
-  if (databaseErrors) return databaseErrors;
-  if (error.message)
-    return {
-      status: 400,
-      message: `${capitalizeFirstLetter(error.message)}.`,
-    };
+  const dbError = getDbError(error);
+  if (dbError) return dbError;
 
   return DEFAULT_ERROR;
 };
+
 const logError = (error, errorData) => {
   if (Object.prototype.hasOwnProperty.call(errorData, 'validationErrors')) {
     console.error(`status: ${errorData.status}, validationErrors:`);
